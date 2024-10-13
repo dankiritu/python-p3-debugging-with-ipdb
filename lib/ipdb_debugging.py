58378bcfcdd3e5abcd84c53c3964fe17ca048630
dankiritu <daniel.kiritu@student.moringaschool.com>
2024-10-13 22:07:23 +0300

    done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-import ipdb
+#import ipdb
 
-def plus_two(num):
-    num + 2
-    return num
+#def plus_two(num):
+ #   num + 2
+  #  return num
+
+def plus_two(number):
+    """Adds 2 to the input number and returns the result."""
+    result = number + 2
+    return result
